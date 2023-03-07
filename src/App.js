d110f04143ed52f6c05eda4e2973f8003bc901f7
mariata06 <slinkywizard@mail.ru>
2023-03-07 17:55:10 +0300

    Add simple markup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,31 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 function App() {
   return (
     <div className="App">
-      <p>Hello React!</p>
+      <div className='calculator-display'>
+        <div className='spot'>1</div>
+        <div className='spot'>1</div>
+        <div className='spot'>+</div>
+        <div className='total'>2</div>
+      </div>
+
+      <div className='calculator-buttons'>
+        <div className='buttons numbers'>
+          {Array(10).fill(0).map((n, i) => (
+            <div key={i} className="number">
+              {i + 1}
+            </div> 
+          ))}
+        </div>
+
+        <div className='buttons operators'>
+          {['*', '-', '+', '/'].map((n, i) => (
+            <div key={i} className="operator">
+              {n}
+            </div>
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 }
