e7a20c289cc3206ce509a86472e109916178eb01
mariata06 <slinkywizard@mail.ru>
2023-03-07 12:44:56 +0300

    Init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useDrag } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello React!</p>
+    </div>
+  );
+}
+
+export default App;
